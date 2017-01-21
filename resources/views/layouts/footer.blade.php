08f6fc0b7eaccfd24d6791f51de661d7748fa23e
addeye <mokhamad27@gmail.com>
2017-01-21 07:34:54 +0700

    first init

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: deyelovi
+ * Date: 21/01/2017
+ * Time: 7:31
+ */
+?>
+<footer class="main-footer">
+    <!-- To the right -->
+    <div class="pull-right hidden-xs">
+        Anything you want
+    </div>
+    <!-- Default to the left -->
+    <strong>Copyright © 2015 <a href="#">Company</a>.</strong> All rights reserved.
+</footer>
