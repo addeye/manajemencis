a207cde010089d1cb974f6900d12d9a926ade722
addeye <mokhamad27@gmail.com>
2017-01-22 10:13:23 +0700

    add template admin LTE

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,4 @@
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('admin', function () {
-    return view('admin_template');
-});
+Route::get('test', 'TestController@index');
